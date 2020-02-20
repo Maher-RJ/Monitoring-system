b6ea34822ec9fe52dfc03da0335a0e6a5abb6e33
MaherRiyadh <maher.jabbar92@gmail.com>
2020-02-20 02:14:21 +0100

    R

--- Laravel/app/Status.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App;
-use Illuminate\Database\Eloquent\Model;
-
-class Status extends Model
-{
-}
--- Laravel/resources/views/partial/charts/custom.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="row">
-    <div class="col-md-12">
-        <div id="container" style="border: 1px black solid;width:100%"></div>
-    </div>
-</div>
